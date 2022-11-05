2c7a79fbc150c4f2309182fd61319f274b64de1c
Arindam Bhattacharjee <51741103+arindam-vbfreak@users.noreply.github.com>
2022-11-05 19:15:01 +0530

    Update nav.php

--- views/partials/nav.php
+++ views/partials/nav.php
@@ -1,4 +1,4 @@
 <nav class="navbar navbar-expand navbar-dark bg-dark shadow-sm">
-  <a class="navbar-brand" href="/index">Paite Dictionary</a>
+  <a class="navbar-brand" href="/index">Web Application</a>
   <span class="fa fa-user text-muted"></span>
 </nav>
